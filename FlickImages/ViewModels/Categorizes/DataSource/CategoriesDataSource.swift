54eb6141191f1aa18d14d45aad3d066978d98217
Hoang Pham <pham.hoang1591@gmail.com>
2021-04-24 10:54:35 +0300

    create generic dataSource class for categories table views

---
+++ FlickImages/ViewModels/Categorizes/DataSource/CategoriesDataSource.swift
@@ -0,0 +1,48 @@
+//
+//  CategoriesDataSource.swift
+//  FlickImages
+//
+//  Created by Hoang Pham on 23.4.2021.
+//
+
+import UIKit
+
+// ViewModel in categories has to implement this protocol
+protocol CategoriesDataSourceProtocol {
+    associatedtype T
+    
+    func numberOfItems() -> Int
+    func getItem(byPosition position: Int) -> T
+}
+
+// Generic dataSource for categories
+class CategoriesDataSource<T: CategoriesDataSourceProtocol>: NSObject, UITableViewDataSource {
+    
+    private var categories: T
+    
+    init(categories: T) {
+        self.categories = categories
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.numberOfItems()
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CategoryCell.reuseId, for: indexPath) as! CategoryCell
+        
+        let category = categories.getItem(byPosition: indexPath.row)
+        
+        // This part might be improved
+        if let category = category as? CategoryProtocol {
+            cell.set(categorizeName: category.title, accessoryType: .disclosureIndicator)
+        } else if let keyword = category as? String {
+            cell.set(categorizeName: keyword, accessoryType: .none)
+        }
+        
+        return cell
+    }
+    
+}
