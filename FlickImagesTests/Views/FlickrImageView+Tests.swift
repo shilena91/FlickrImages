31711deeb35057490027749b43b2c56c451afe88
Hoang Pham <pham.hoang1591@gmail.com>
2021-04-29 10:16:10 +0300

    create FlickrImageView+Tests

---
+++ FlickImagesTests/Views/FlickrImageView+Tests.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoCell+Tests.swift
+//  FlickImagesTests
+//
+//  Created by Hoang Pham on 28.4.2021.
+//
+
+import XCTest
+@testable import FlickImages
+
+class FlickrImageView_Tests: XCTestCase {
+    
+    private var flickrImageView: FlickrImageView?
+
+    override func setUp() {
+        flickrImageView = FlickrImageView(frame: .zero)
+        super.setUp()
+    }
+
+    override func tearDown() {
+        flickrImageView = nil
+        super.tearDown()
+    }
+
+    // This test is flankiness, should be improved
+    func testDownloadImageFail() {
+        let promise = expectation(description: "download Image Fail")
+        
+        let dumbURLString = "https://live.staticflickr.com/65535/51140125339_8ede520502.jpn"
+        
+        flickrImageView?.downloadImage(from: dumbURLString)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if self.flickrImageView?.image == Images.placeholder {
+                promise.fulfill()
+            }
+            
+        }
+        
+        wait(for: [promise], timeout: 3)
+    }
+  
+}
